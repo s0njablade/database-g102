ca0eb2acf36321fcc5283f2126177f79a01cad07
s0njablade <sonjita.chacon@gmail.com>
2018-12-05 10:46:16 -0700

    add students route file

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const port = process.env.port || 3004
+const students = require('./students')
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
+app.use('/students', students)
 
 app.listen(port, () => {
     console.log(`running on port: ${port}`)
